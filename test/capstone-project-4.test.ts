6f90833223695ad0a4bd3bc6812c3b02ac6723dc
omaomach <aaronjoash44@gmail.com>
2026-05-04 07:07:29 +0300

    Initial commit

---
+++ test/capstone-project-4.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CapstoneProject4 from '../lib/capstone-project-4-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/capstone-project-4-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CapstoneProject4.CapstoneProject4Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
